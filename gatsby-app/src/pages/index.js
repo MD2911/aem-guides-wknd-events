e0f07bce1bdfacf0f8584fcf85c477cf9b3c595a
MD2911 <daud.wong@gmail.com>
2019-10-14 15:11:10 -0500

    Progressing with gatsby integration

--- gatsby-app/src/pages/index.js
+++ gatsby-app/src/pages/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import { ModelManager, Constants } from '@adobe/cq-spa-page-model-manager';
-import '../components/App.js'
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import ScrollToTop from '../components/utils/RouteHelper';
+import App from '../components/App'
+import { Redirect, Route } from 'react-router';
 
+console.log("Path = " + process.env.GATSBY_APP_PAGE_MODEL_PATH );
+//ModelManager.initialize({ path: process.env.GATSBY_APP_PAGE_MODEL_PATH });
 
-const IndexPage = () => (
-  <div>Hello</div>
-)
+let path = "";
+let root = <div></div>;
+let model = null;
 
-export default IndexPage
+if (typeof window !== 'undefined') {
+  path =  window.location.pathname ;
+}
+
+ if (typeof document !== 'undefined') {
+  root = document.getElementById('root');
+}
+
+// This component is the application entry point
+export default class IndexPage extends React.Component {
+
+    render() {
+        // create a async for later to handle with await
+        async function initModelManager() {
+            let p = ModelManager.initialize({ path: process.env.GATSBY_APP_PAGE_MODEL_PATH }).then((aemmodel) => {
+                    model = aemmodel;
+                }
+            );
+
+            console.log(p);
+
+            // wait until init finished
+            await p;
+        }
+
+        initModelManager().catch(() => { console.log ("Error in fetching....")});
+
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
